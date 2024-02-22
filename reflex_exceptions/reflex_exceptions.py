b594563c22cddb073028b33b5106fdf8e2f184fa
Tanvir Riyad <113178352+tanvirriyad@users.noreply.github.com>
2024-02-22 13:39:33 +0100

    exception test repo

---
+++ reflex_exceptions/reflex_exceptions.py
@@ -0,0 +1,33 @@
+import reflex as rx
+
+
+class State(rx.State):
+    count: int = 0
+
+    def increment(self):
+        self.count += 1
+        if self.count % 2 == 0:
+            raise ValueError("Even number detected!")
+
+
+def index() -> rx.Component:
+    return rx.center(
+        rx.theme_panel(),
+        rx.vstack(
+            rx.heading("Welcome to Reflex!", size="9"),
+            rx.text(f"Count: {State.count}"),
+            rx.button(
+                "Increment",
+                on_click=State.increment,  # type: ignore
+                size="4",
+            ),
+            align="center",
+            spacing="7",
+            font_size="2em",
+        ),
+        height="100vh",
+    )
+
+
+app = rx.App()
+app.add_page(index)
